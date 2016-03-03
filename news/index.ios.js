2ea9f64ef02f1e32ced297744458bec2ce3cd815
林溢泽 <linyize@gmail.com>
2016-03-03 18:14:57 +0800

    网易tabbar图标

--- news/index.ios.js
+++ news/index.ios.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var React = require('react-native');
-var { AppRegistry, Navigator, Text, View, StyleSheet, PropTypes, NativeAppEventEmitter } = React;
+var { AppRegistry, Navigator, Text, View, Image, StyleSheet, PropTypes, NativeAppEventEmitter } = React;
 var { Actions, Router, Route, Schema, TabBar } = require('react-native-router-flux');
 
 // 主界面
@@ -11,10 +11,34 @@ var Launch = React.createClass({
     }
 });
 
-class TabIcon extends React.Component {
+class NeteaseNewsTabIcon extends React.Component {
     render(){
+        var icon;
+        if (this.props.name === 'news') {
+            icon = this.props.selected ? require('./image/tabbar_icon_news_highlight.png') 
+                        : require('./image/tabbar_icon_news_normal.png');
+        }
+        else if (this.props.name === 'reader') {
+            icon = this.props.selected ? require('./image/tabbar_icon_reader_highlight.png') 
+                        : require('./image/tabbar_icon_reader_normal.png');
+        }
+        else if (this.props.name === 'media') {
+            icon = this.props.selected ? require('./image/tabbar_icon_media_highlight.png') 
+                        : require('./image/tabbar_icon_media_normal.png');
+        }
+        else if (this.props.name === 'bar') {
+            icon = this.props.selected ? require('./image/tabbar_icon_bar_highlight.png') 
+                        : require('./image/tabbar_icon_bar_normal.png');
+        }
+        else if (this.props.name === 'me') {
+            icon = this.props.selected ? require('./image/tabbar_icon_me_highlight.png') 
+                        : require('./image/tabbar_icon_me_normal.png');
+        }
         return (
+            <View>
+            <Image source={icon}></Image>
             <Text style={{color: this.props.selected ? 'red' :'black'}}>{this.props.title}</Text>
+            </View>
         );
     }
 }
@@ -34,14 +58,14 @@ class News extends React.Component {
         return (
             <Router>
                 <Schema name="default" sceneConfig={Navigator.SceneConfigs.FloatFromRight}/>
-                <Schema name="tab" type="switch" icon={TabIcon} />
+                <Schema name="tab" type="switch" icon={NeteaseNewsTabIcon} />
                 <Route name="tabbar">
                     <Router footer={TabBar} showNavigationBar={false}>
-                        <Route name="tab1" schema="tab" title="新闻" component={Launch}/>
-                        <Route name="tab2" schema="tab" title="阅读" component={Launch}/>
-                        <Route name="tab3" schema="tab" title="视听" component={Launch}/>
-                        <Route name="tab4" schema="tab" title="话题" component={Launch}/>
-                        <Route name="tab5" schema="tab" title="我" component={Launch}/>
+                        <Route name="news" schema="tab" title="新闻" component={Launch}/>
+                        <Route name="reader" schema="tab" title="阅读" component={Launch}/>
+                        <Route name="media" schema="tab" title="视听" component={Launch}/>
+                        <Route name="bar" schema="tab" title="话题" component={Launch}/>
+                        <Route name="me" schema="tab" title="我" component={Launch}/>
                     </Router>
                 </Route>
             </Router>
