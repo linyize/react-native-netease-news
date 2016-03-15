caf88ab9118d90f0433f1410dda81b072c3396e8
林溢泽 <linyize@gmail.com>
2016-03-15 11:20:45 +0800

    实现多栏目页滑动，与导航条联动

--- news/Netease/ColumnBar.js
+++ news/Netease/ColumnBar.js
@@ -5,25 +5,35 @@ var { Text, View, Image, StyleSheet, TouchableOpacity } = React;
 var { Actions } = require('react-native-router-flux');
 
 var ColumnBar = React.createClass({
+    propTypes: {
+        onChangeTabs: React.PropTypes.func
+    },
+
+    onPressTab(index) {
+      this.props.onChangeTabs(index);
+    },
+
     render(){
+
+      var index = this.props.index;
         return <View style={styles.container}>
-        			<TouchableOpacity style={styles.button}>
-        				<Text style={styles.buttonTitleHighlight}>头条</Text>
+        			<TouchableOpacity style={styles.button} onPress={()=>this.onPressTab(0)}>
+        				<Text style={index == 0 ? styles.buttonTitleHighlight : styles.buttonTitle}>头条</Text>
         			</TouchableOpacity>
-        			<TouchableOpacity style={styles.button}>
-        				<Text style={styles.buttonTitle}>体育</Text>
+        			<TouchableOpacity style={styles.button} onPress={()=>this.onPressTab(1)}>
+        				<Text style={index == 1 ? styles.buttonTitleHighlight : styles.buttonTitle}>体育</Text>
         			</TouchableOpacity>
-        			<TouchableOpacity style={styles.button}>
-        				<Text style={styles.buttonTitle}>财经</Text>
+        			<TouchableOpacity style={styles.button} onPress={()=>this.onPressTab(2)}>
+        				<Text style={index == 2 ? styles.buttonTitleHighlight : styles.buttonTitle}>财经</Text>
         			</TouchableOpacity>
-        			<TouchableOpacity style={styles.button}>
-        				<Text style={styles.buttonTitle}>汽车</Text>
+        			<TouchableOpacity style={styles.button} onPress={()=>this.onPressTab(3)}>
+        				<Text style={index == 3 ? styles.buttonTitleHighlight : styles.buttonTitle}>汽车</Text>
         			</TouchableOpacity>
-        			<TouchableOpacity style={styles.button}>
-        				<Text style={styles.buttonTitle}>热点</Text>
+        			<TouchableOpacity style={styles.button} onPress={()=>this.onPressTab(4)}>
+        				<Text style={index == 4 ? styles.buttonTitleHighlight : styles.buttonTitle}>热点</Text>
         			</TouchableOpacity>
-        			<TouchableOpacity style={styles.button}>
-        				<Text style={styles.buttonTitle}>订阅</Text>
+        			<TouchableOpacity style={styles.button} onPress={()=>this.onPressTab(5)}>
+        				<Text style={index == 5 ? styles.buttonTitleHighlight : styles.buttonTitle}>订阅</Text>
         			</TouchableOpacity>
                 </View>;
     }
--- news/Netease/News.js
+++ news/Netease/News.js
@@ -3,16 +3,48 @@
 var React = require('react-native');
 var { Text, View, Image, StyleSheet, TouchableOpacity } = React;
 var { Actions } = require('react-native-router-flux');
-
+var Button = require('react-native-button');
+import SwipeableViews from 'react-swipeable-views/lib/index.native.animated';
 var Column = require('../choice/Column');
 var ColumnBar = require('./ColumnBar');
 
 var News = React.createClass({
+    getInitialState() {
+        return {
+            index: 0
+        };
+    },
+
+    handleChangeTabs(value : Number) {
+        this.setState({
+            index: value,
+        });
+    },
+
+    handleChangeIndex(index : Number) {
+        this.setState({
+            index: index,
+        });
+    },
+
     render(){
+        var index = this.state.index;
+
         return <View style={styles.container}>
-                    <ColumnBar />
+                <ColumnBar index={index} onChangeTabs={this.handleChangeTabs} />
+                <SwipeableViews
+                    index={index}
+                    onChangeIndex={this.handleChangeIndex}
+                    containerStyle={styles.slideContainer}
+                >
                     <Column />
-                </View>;
+                    <Column />
+                    <Column />
+                    <Column />
+                    <Column />
+                    <Column />
+                </SwipeableViews>
+            </View>;
     }
 });
 
@@ -22,6 +54,25 @@ var styles = StyleSheet.create({
     flexDirection: 'column',
     backgroundColor: 'lightgray',
     marginTop: 64
+  },
+  slideContainer: {
+    flex: 1,
+  },
+  slide: {
+    flex: 1,
+  },
+  slide1: {
+    backgroundColor: '#FEA900',
+  },
+  slide2: {
+    backgroundColor: '#B3DC4A',
+  },
+  slide3: {
+    backgroundColor: '#6AC0FF',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 16,
   }
 });
 
